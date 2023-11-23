86bcd44913549753c0bdc92c3d2ebffa722b3f23
DeepakSR-12 <deepakdeepakeng@gmail.com>
2023-11-23 23:24:38 +0530

    fix: api key fetch issue

--- background.js
+++ background.js
@@ -1,23 +1,22 @@
-const getStorageData = (key) => {
-  return new Promise((resolve, reject) => {
-    chrome.storage.sync.get(key, (data) => {
-      if (chrome.runtime.lastError) {
-        reject(chrome.runtime.lastError);
-      } else {
-        resolve(data[key]);
-      }
-    });
-  });
-};
-
-chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.type === "getKeys") {
-    try {
+    const getStorageData = (key) => {
+      return new Promise((resolve) => {
+        chrome.storage.sync.get(key, (data) => {
+          resolve(data[key]);
+        });
+      });
+    };
+
+    const handleGetKeys = async () => {
       const apiKey = await getStorageData("apiKey");
+
       sendResponse({ apiKey });
-    } catch (error) {
-      sendResponse({ error: "Failed to fetch apiKey" });
-    }
+    };
+
+    handleGetKeys();
+
+    // Keep the listener open for the async response
     return true;
   }
 });
--- content.js
+++ content.js
@@ -6,7 +6,7 @@ const openAiApiCall = async (apiKey, prompt) => {
       Authorization: `Bearer ${apiKey}`,
     },
     body: JSON.stringify({
-      model: "gpt-3.5-turbo",
+      model: "gpt-3.5-turbo-1106",
       messages: [{ role: "assistant", content: prompt }],
       temperature: 0.7,
     }),
@@ -20,70 +20,21 @@ const openAiApiCall = async (apiKey, prompt) => {
   return data.choices[0].message.content.trim();
 };
 
-const updateInput = async (regex, callback) => {
-  let updatedText = inputText;
-  let match;
-  let matchFound = false;
-
-  while ((match = regex.exec(updatedText)) !== null) {
-    matchFound = true;
-    const generatedText = await callback(match);
-    updatedText = updatedText.replace(match[0], generatedText);
-
-    // Reset lastIndex to 0 to avoid infinite loops
-    regex.lastIndex = 0;
-  }
-  if (matchFound) {
-    // Simulate typing the updatedText in the Twitter input field
-    const activeElement = document.activeElement;
-    const oldText = activeElement.textContent;
-    activeElement.textContent = "";
-    activeElement.focus();
-
-    document.execCommand("insertText", false, updatedText);
-    activeElement.dispatchEvent(new Event("change", { bubbles: true }));
-
-    // If the input field is not updated, fallback to the old method
-    if (activeElement.textContent !== updatedText) {
-      activeElement.textContent = oldText;
-    }
-
-    target.value = updatedText; //General
-    document.activeElement.innerText = updatedText; //Linkedin
-  }
-};
-
-const generateText = async (query) => {
-  console.log("query: ", query);
-  return await openAiApiCall(apiKey, query);
-};
-
-const translateText = async (language, query) => {
-  console.log("language: ", language);
-  const prompt = `Translate the following English text to ${language}: "${query}"`;
-  return await openAiApiCall(apiKey, prompt);
-};
-
-const generateCode = async (language, query) => {
-  console.log("programming language: ", language);
-  const prompt = `Write ${language} code to ${query}`;
-  return await openAiApiCall(apiKey, prompt);
-};
-
 chrome.runtime.sendMessage({ type: "getKeys" }, async (response) => {
+  const apiKey = response.apiKey;
+
+  if (!apiKey) {
+    alert("Please set your OpenAI API key in the extension options.");
+    return;
+  }
+
   document.addEventListener("keyup", async (event) => {
-    const apiKey = response.apiKey;
-
-    if (!apiKey) {
-      alert("Please set your OpenAI API key in the extension options.");
-      return;
-    }
-
     if (event.key !== ";") {
       return;
     }
 
-    let inputText = event.target.value;
+    const target = event.target;
+    let inputText = target.value;
 
     if (inputText === "" || inputText === undefined) {
       inputText = document.activeElement.innerText; // LinkedIn
@@ -96,6 +47,53 @@ chrome.runtime.sendMessage({ type: "getKeys" }, async (response) => {
     const helpRegex = /help:\s*(.+?);/g;
     const langRegex = /translate,([a-zA-Z]+):\s*(.+?);/g;
     const codeRegex = /code,([a-zA-Z]+):\s*(.+?);/g;
+
+    const updateInput = async (regex, callback) => {
+      let updatedText = inputText;
+      let match;
+      let matchFound = false;
+
+      while ((match = regex.exec(updatedText)) !== null) {
+        matchFound = true;
+        const generatedText = await callback(match);
+        updatedText = updatedText.replace(match[0], generatedText);
+
+        // Reset lastIndex to 0 to avoid infinite loops
+        regex.lastIndex = 0;
+      }
+      if (matchFound) {
+        // Simulate typing the updatedText in the Twitter input field
+        const activeElement = document.activeElement;
+        const oldText = activeElement.textContent;
+        activeElement.textContent = "";
+        activeElement.focus();
+
+        document.execCommand("insertText", false, updatedText);
+        activeElement.dispatchEvent(new Event("change", { bubbles: true }));
+
+        // If the input field is not updated, fallback to the old method
+        if (activeElement.textContent !== updatedText) {
+          activeElement.textContent = oldText;
+        }
+
+        target.value = updatedText; //General
+        document.activeElement.innerText = updatedText; //Linkedin
+      }
+    };
+
+    const generateText = async (query) => {
+      return await openAiApiCall(apiKey, query);
+    };
+
+    const translateText = async (language, query) => {
+      const prompt = `Translate the following English text to ${language}: "${query}"`;
+      return await openAiApiCall(apiKey, prompt);
+    };
+
+    const generateCode = async (language, query) => {
+      const prompt = `Write ${language} code to ${query}`;
+      return await openAiApiCall(apiKey, prompt);
+    };
 
     await updateInput(helpRegex, (match) => generateText(match[1]));
     await updateInput(langRegex, (match) => translateText(match[1], match[2]));
